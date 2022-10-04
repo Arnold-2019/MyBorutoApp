cba5f3e6e0c9d19ccce8b6c2687497cfbe46f15b
Arnold Wang <arnold.wang@rea-group.com>
2022-10-04 22:43:37 +0800

    feat[5]: create HeroRemoteKey and Dao

--- app/src/main/java/com/example/myborutoapp/data/local/BorutoDatabase.kt
+++ app/src/main/java/com/example/myborutoapp/data/local/BorutoDatabase.kt
@@ -3,10 +3,13 @@ package com.example.myborutoapp.data.local
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.example.myborutoapp.data.local.dao.HeroDao
+import com.example.myborutoapp.data.local.dao.HeroRemoteKeyDao
 import com.example.myborutoapp.domain.model.Hero
+import com.example.myborutoapp.domain.model.HeroRemoteKey
 
-@Database(entities = [Hero::class], version = 1)
+@Database(entities = [Hero::class, HeroRemoteKey::class], version = 1)
 abstract class BorutoDatabase : RoomDatabase() {
 
     abstract fun heroDao(): HeroDao
+    abstract fun heroRemoteKeyDao(): HeroRemoteKeyDao
 }
