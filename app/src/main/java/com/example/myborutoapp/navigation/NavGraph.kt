7e0069e3fcc9756b95e699a30af8482bbc083b38
Arnold Wang <arnold.wang@rea-group.com>
2022-10-05 21:48:34 +0800

    feat[8]: create splash screen

--- app/src/main/java/com/example/myborutoapp/navigation/NavGraph.kt
+++ app/src/main/java/com/example/myborutoapp/navigation/NavGraph.kt
@@ -6,6 +6,7 @@ import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.navArgument
+import com.example.myborutoapp.presentation.screens.splash.SplashScreen
 import com.example.myborutoapp.util.Constants.DETAILS_ARGUMENT_KEY
 
 @Composable
@@ -16,7 +17,7 @@ fun SetUpNavGraph(navHostController: NavHostController) {
         startDestination = Screen.Welcome.route
     ) {
         composable(route = Screen.Welcome.route) {
-
+            SplashScreen(navHostController = navHostController)
         }
         composable(route = Screen.Home.route) {
 
