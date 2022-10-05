338abd6213ade1b2489423d679491f70a95d4d11
Arnold Wang <arnold.wang@rea-group.com>
2022-10-05 11:15:06 +0800

    feat[7]: setup dagger hilt

--- app/src/main/java/com/example/myborutoapp/MainActivity.kt
+++ app/src/main/java/com/example/myborutoapp/MainActivity.kt
@@ -7,7 +7,9 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import com.example.myborutoapp.navigation.SetUpNavGraph
 import com.example.myborutoapp.ui.theme.MyBorutoAppTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 
     private lateinit var navHostController: NavHostController
---
+++ app/src/main/java/com/example/myborutoapp/di/DatabaseModule.kt
@@ -0,0 +1,27 @@
+package com.example.myborutoapp.di
+
+import android.content.Context
+import androidx.room.Room
+import com.example.myborutoapp.data.local.BorutoDatabase
+import com.example.myborutoapp.util.Constants.BORUTO_DATABASE
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+class DatabaseModule {
+
+    @Provides
+    @Singleton
+    fun provideDatabase(
+        @ApplicationContext context: Context
+    ) = Room.databaseBuilder(
+        context,
+        BorutoDatabase::class.java,
+        BORUTO_DATABASE
+    ).build()
+}
