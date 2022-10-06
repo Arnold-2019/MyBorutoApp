f9f7c8595bd26f1b1b28459a540cd8f6ab61fd06
Arnold Wang <arnold.wang@rea-group.com>
2022-10-06 16:04:00 +0800

    feat[13]: create color and dimen utils

--- app/src/main/java/com/example/myborutoapp/ui/theme/Color.kt
+++ app/src/main/java/com/example/myborutoapp/ui/theme/Color.kt
@@ -9,6 +9,17 @@ val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
 
+val LightGray = Color(0xFFD8D8D8)
+val DarkGray = Color(0xFF2A2A2A)
+
 val Colors.welcomeScreenBackgroundColor
     @Composable
     get() = if (isLight) Color.White else Color.Black
+
+val Colors.titleColor
+    @Composable
+    get() = if (isLight) Color.DarkGray else Color.LightGray
+
+val Colors.descriptionColor
+    @Composable
+    get() = if (isLight) Color.DarkGray.copy(alpha = 0.5f) else Color.LightGray.copy(alpha = 0.5f)
