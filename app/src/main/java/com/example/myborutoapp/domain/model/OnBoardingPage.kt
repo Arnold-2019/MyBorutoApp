2c21ce28eb500aea8e02cb1253949d2cf33effc0
Arnold Wang <arnold.wang@rea-group.com>
2022-10-06 15:28:18 +0800

    feat[12]: create on boarding page model

---
+++ app/src/main/java/com/example/myborutoapp/domain/model/OnBoardingPage.kt
@@ -0,0 +1,26 @@
+package com.example.myborutoapp.domain.model
+
+import androidx.annotation.DrawableRes
+import com.example.myborutoapp.R
+
+sealed class OnBoardingPage(
+    @DrawableRes image: Int,
+    tile: String,
+    description: String
+) {
+    object First: OnBoardingPage(
+        image = R.drawable.greetings,
+        tile = "Greetings",
+        description = "Are you a Boruto fan? Because if you are then we have a great news for you!"
+    )
+    object Second: OnBoardingPage(
+        image = R.drawable.explore,
+        tile = "Explore",
+        description = "Find your favorite heroes and learn some of the things that you didn't know about.   "
+    )
+    object Third: OnBoardingPage(
+        image = R.drawable.power,
+        tile = "Power",
+        description = "Check out your hero's power and  see how much are they strong comparing to others."
+    )
+}
