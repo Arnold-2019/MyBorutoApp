1f6e963be126e7b930fa2251ab23159bc8d04134
Arnold Wang <arnold.wang@rea-group.com>
2022-12-04 15:59:52 +0800

    feat[21]: create NetworkModule

---
+++ app/src/main/java/com/example/myborutoapp/di/NetworkModule.kt
@@ -0,0 +1,47 @@
+@file:Suppress("OPT_IN_IS_NOT_ENABLED")
+
+package com.example.myborutoapp.di
+
+import com.example.myborutoapp.data.remote.BorutoApi
+import com.example.myborutoapp.util.Constants.BASE_URL
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import dagger.Module
+import dagger.Provides
+import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
+
+@OptIn(ExperimentalSerializationApi::class)
+@Module
+object NetworkModule {
+
+    @Provides
+    @Singleton
+    fun provideHttpClient(): OkHttpClient {
+        return OkHttpClient.Builder()
+            .readTimeout(15, TimeUnit.MINUTES)
+            .connectTimeout(15, TimeUnit.MINUTES)
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideRetrofitInstance(okHttpClient: OkHttpClient): Retrofit {
+        val converterType = MediaType.get("application/json")
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(okHttpClient)
+            .addConverterFactory(Json.asConverterFactory(converterType))
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideBorutoApi(retrofit: Retrofit): BorutoApi {
+        return retrofit.create(BorutoApi::class.java)
+    }
+}
--- app/src/main/java/com/example/myborutoapp/util/Constants.kt
+++ app/src/main/java/com/example/myborutoapp/util/Constants.kt
@@ -1,6 +1,9 @@
 package com.example.myborutoapp.util
 
 object Constants {
+
+    const val BASE_URL = "http://10.0.2.2:8080"
+
     const val BORUTO_DATABASE = "boruto_database"
     const val DETAILS_ARGUMENT_KEY = "heroId"
     const val HERO_DATABASE_TABLE = "hero_table"
